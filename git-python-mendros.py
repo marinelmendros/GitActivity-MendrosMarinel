aaeb09074bd029f84edd1ba7a977860c9172c7ff
MendrosMarinel <mendrosmarinel@gmail.com>
2022-03-14 18:31:44 +0800

    My Personal Info added

---
+++ git-python-mendros.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 18:14:26 2022
+
+@author: JOMARI
+"""
+
+print("Marinel M. Mendros")
+print("Tagwak, Boac, Marinduque")
+print("January 11, 2002")
