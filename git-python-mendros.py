c3e2fddcc949aa52c3c465c7bfc3f4f8362e3152
MendrosMarinel <mendrosmarinel@gmail.com>
2022-03-14 18:50:57 +0800

    My Educational Background added

--- git-python-mendros.py
+++ git-python-mendros.py
@@ -8,3 +8,6 @@ Created on Mon Mar 14 18:14:26 2022
 print("Marinel M. Mendros")
 print("Tagwak, Boac, Marinduque")
 print("January 11, 2002")
+print("Marinduque State College")
+print("BSI/T")
+print("2B")
